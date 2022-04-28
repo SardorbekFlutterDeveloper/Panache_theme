06f186c2f16b27c7a914fc128ed84906bb4194d4
Sardor Sattorov <95919851+SardorbekFlutterDeveloper@users.noreply.github.com>
2022-04-28 20:45:36 +0500

    Add files via upload

---
+++ color_cons.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static Color kPrimaryColor = const Color(0xff3f51b5);
+  static Color kPrimaryColorLight = const Color(0xffc5cae9);
+  static Color kPrimaryColorDark = const Color(0xff4caf50);
+  static Color kCanvasColor = const Color(0xffffeb3b);
+  static Color kScaffoldBackgroundColor = const Color(0xfffafafa);
+  static Color kBottomAppBarColor = const Color(0xffffffff);
+  static Color kCardColor = const Color(0xffffffff);
+  static Color kDividerColor = const Color(0x1f000000);
+  static Color kHighlightColor = const Color(0x66bcbcbc);
+  static Color kSplashColor = const Color(0x66c8c8c8);
+  static Color kSelectedRowColor = const Color(0xfff5f5f5);
+  static Color kUnselectedWidgetColor = const Color(0x8a000000);
+  static Color kDisabledColor = const Color(0x61000000);
+  static Color kToggleableActiveColor = const Color(0xff3949ab);
+  static Color kSecondaryHeaderColor = Color(0xffe8eaf6);
+  static Color kBackgroundColor = Color(0xff9fa8da);
+  static Color kDialogBackgroundColor = Color(0xffffffff);
+  static Color kIndicatorColor = Color(0xff3f51b5);
+  static Color kHintColor = Color(0x8a000000);
+  static Color kErrorColor = Color(0xffd32f2f);
+  static Color kSecondaryTextColor = Color(0xff000000);
+  static Color kMainTextColor = Color(0xffff5722);
+  static Color kBodyTextColor = Color(0xff00bcd4);
+  
+}
